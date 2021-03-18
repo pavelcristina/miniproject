a3c5e6e2673171063cfa10a90f8cc56da4f74b79
Dell <pavel.cristina.maria16@gmail.com>
2021-03-18 16:22:24 +0100

    fix regular expression

--- main.js
+++ main.js
@@ -45,8 +45,8 @@ function outputData(person) {
     row.classList.add('actor');
   }
   // Add a class to Austrians
-  if ((new RegExp(/austria/gi)).test(person.profession)) {
-    row.classList.add('austrian');
+  if ((new RegExp(/Austria/gi)).test(person.profession)) {
+    row.classList.add('Austria');
   }
   // Add a class to Auto*
   if ((new RegExp(/author/gi)).test(person.profession)) {
